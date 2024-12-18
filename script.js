76a18a9767d031c97fede871feacedbbe7805729
21102284 <123989282+HanifRafiq372@users.noreply.github.com>
2024-12-18 14:54:57 +0700

    PAAS

---
+++ script.js
@@ -0,0 +1,14 @@
+function calculate() {
+    const radius = document.getElementById("radius").value;
+    if (!radius || radius <= 0) {
+      alert("Masukkan jari-jari yang valid!");
+      return;
+    }
+  
+    const circumference = (2 * Math.PI * radius).toFixed(2);
+    const area = (Math.PI * radius * radius).toFixed(2);
+  
+    document.getElementById("circumference").innerText = `Keliling Lingkaran: ${circumference} cm`;
+    document.getElementById("area").innerText = `Luas Lingkaran: ${area} cm²`;
+  }
+  
